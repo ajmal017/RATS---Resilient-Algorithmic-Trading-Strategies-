17ebf1bef7715e4b86b1836a0425afb77e79782a
bonnerke <55808159+bonnerke@users.noreply.github.com>
2021-02-07 16:29:02 -0800

    Add files via upload

--- RATS_application/backend/apps/quant_connect/urls.py
+++ RATS_application/backend/apps/quant_connect/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/Tests', views.get_tests, name='get_tests'),
+    path('Tests', views.get_tests, name='get_tests'),
     url(r'algorithm_manager/set_algorithm',
         views.set_algorithm,
         name="algorithm-manager-set-algorithm"),
+    path('getdb', views.getdb, name='getdb')
 ]
--- RATS_application/backend/apps/quant_connect/views.py
+++ RATS_application/backend/apps/quant_connect/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from rest_framework.decorators import api_view
+from django.http import HttpResponse
 from .components.tests import Test
 from django.views.decorators.csrf import csrf_exempt
 from .components.algorithm_manager import AlgorithmManager
+from .models import *
+from django.core import serializers
+from django.http import JsonResponse
 
 
 def index(request):
@@ -12,6 +15,16 @@ def index(request):
 
 def get_tests(request):
     return Tests().get_tests(request, 'quant_connect.html')
+
+def getdb(request):
+    users = User.objects.all()
+    backtests = Backtest.objects.all()
+    print(users, backtests)
+    json_data = serializers.serialize('json', backtests)
+    return JsonResponse(json_data, safe = False)
+
+
+
 
 
 @api_view(['POST'])
